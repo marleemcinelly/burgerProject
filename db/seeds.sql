9dee88361055dc00177ea1a1ce523ee4fed2c8a6
Marlee McInelly <marleemcinelly@gmail.com>
2021-02-22 16:13:32 -0700

    added general code, does not function as intended

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(1,"Cheeseburger", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(2,"Bacon Cheeseburger", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(3,"Veggie Burger", false);
